c17d3788bab882a9b1a729e3558fa08149f436cf
Andrii Tsokol <atsokol@gmail.com>
2026-01-29 21:06:58 +0100

    Update base path for bess-model-dashboard

--- observablehq.config.js
+++ observablehq.config.js
@@ -3,7 +3,7 @@ export default {
   // The app’s title; used in the sidebar and webpage titles.
   title: "Ukraine BESS revenue model",
   // Set base path for GitHub Pages deployment
-  base: "/bess-model",
+  base: "/bess-model-dashboard",
   // The pages and sections in the sidebar. If you don’t specify this option,
   // all pages will be listed in alphabetical order. Listing pages explicitly
   // lets you organize them into sections and have unlisted pages.
